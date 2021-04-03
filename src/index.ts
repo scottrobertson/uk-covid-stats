79bbc0ef23e1817630bb1f281b5ee2ddb05c0de4
Scott Robertson <scottymeuk@gmail.com>
2021-04-03 16:13:05 +0100

    Combine graphs, and add options to enable graph and all-time data

--- src/index.ts
+++ src/index.ts
@@ -8,45 +8,60 @@ class UkCovidStats extends Command {
   static flags = {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
+    graph: flags.boolean({char: 'g', default: false}),
+    'all-time': flags.boolean({char: 'a', default: false}),
   }
 
   async run() {
+    const {flags} = this.parse(UkCovidStats)
+
     const rolloutUrl = 'https://coronavirus.data.gov.uk/api/v1/data?filters=areaName=United%2520Kingdom;areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22cumVaccinationFirstDoseUptakeByPublishDatePercentage%22:%22cumVaccinationFirstDoseUptakeByPublishDatePercentage%22,%22cumVaccinationSecondDoseUptakeByPublishDatePercentage%22:%22cumVaccinationSecondDoseUptakeByPublishDatePercentage%22%7D&format=json'
-
     const rolloutResponse = await axios.get(rolloutUrl)
+    const rolloutData = orderBy(rolloutResponse.data.data, ['date', 'asc'])
+    let limitedRolloutData
 
-    let rolloutData = orderBy(rolloutResponse.data.data, ['date', 'asc'])
-    rolloutData = rolloutData.slice(Math.max(rolloutData.length - 30, 1))
+    if (flags['all-time']) {
+      limitedRolloutData = rolloutData
+    } else {
+      limitedRolloutData = rolloutData.slice(Math.max(rolloutData.length - 30, 1))
+    }
 
     const rolloutTable = new Table({
       head: ['Date', 'First Dose %', 'Second Dose %'],
       colWidths: [15, 15, 16],
     })
 
-    const firstDoseChart = new Array(rolloutData.length)
-    const secondDoseChart = new Array(rolloutData.length)
-
-    rolloutData.forEach((row: any, index: number) => {
-      firstDoseChart[index] = row.cumVaccinationFirstDoseUptakeByPublishDatePercentage
-      secondDoseChart[index] = row.cumVaccinationSecondDoseUptakeByPublishDatePercentage
-
+    limitedRolloutData.forEach((row: any) => {
       rolloutTable.push([row.date, row.cumVaccinationFirstDoseUptakeByPublishDatePercentage, row.cumVaccinationSecondDoseUptakeByPublishDatePercentage])
     })
 
     this.log('')
-    this.log('Last 30 days of UK covid vaccine data')
-    this.log('')
     this.log(rolloutTable.toString())
 
-    this.log('')
-    this.log('1st Dose Over Time')
-    this.log('')
-    this.log(asciichart.plot(firstDoseChart))
+    if (flags.graph) {
+      const firstDoseChart = new Array(limitedRolloutData.length)
+      const secondDoseChart = new Array(limitedRolloutData.length)
 
-    this.log('')
-    this.log('2nd Dose Over Time')
-    this.log('')
-    this.log(asciichart.plot(secondDoseChart))
+      limitedRolloutData.forEach((row: any, index: number) => {
+        firstDoseChart[index] = row.cumVaccinationFirstDoseUptakeByPublishDatePercentage
+        secondDoseChart[index] = row.cumVaccinationSecondDoseUptakeByPublishDatePercentage
+      })
+
+      this.log('')
+      this.log('Dose %')
+      this.log('- 1st Dose: Blue')
+      this.log('- 2nd Dose: Green')
+      this.log('')
+
+      const config = {
+        colors: [
+          asciichart.blue,
+          asciichart.green,
+        ],
+      }
+
+      this.log(asciichart.plot([firstDoseChart, secondDoseChart], config))
+    }
   }
 }
 
